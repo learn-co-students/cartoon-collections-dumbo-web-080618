253d08f7a7116ea3384a0e45f646a4fb1f09a8d8
mikepcarlos <michaelpcarlos@gmail.com>
2018-07-24 11:08:32 -0400

    cartoon loops

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,22 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(names)
+  names.each_with_index { |dwarf, index|
+    puts "#{index + 1} #{dwarf}"
+  }
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(calls)
+  calls.collect do |call|
+    call.capitalize + "!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  calls.any? do |word|
+    word.length > 4
+  end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(str)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  str.find { |cheese| cheese_types.include?(cheese)}
 end
